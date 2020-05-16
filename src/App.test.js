343294851bd89c0b90ebd6b6e1a0f01d1c5c00e2
duxinzhe <naomixzdu@gmail.com>
2020-05-16 15:28:28 -0500

    add YAML file

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import Chore from './components/Chore'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   const { getByText } = render(<Chore />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('starter test', () => {
+  const value = 1;
+  expect(value).toEqual(1);
 });
